a284491938911048dd7ee28c5e6ac6323f381df0
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-05-28 17:46:48 -0700

    Initial content

---
+++ app/src/main/java/org/fbluemle/as400/MainActivity.java
@@ -0,0 +1,13 @@
+package org.fbluemle.as400;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
